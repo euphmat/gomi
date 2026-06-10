04e1d03bee2b3079c7213374693267705071f971
euphmat <euphmat@gmail.com>
2026-06-10 21:41:49 +0900

    commit

--- js/definitions/dungeons.js
+++ js/definitions/dungeons.js
@@ -9,8 +9,8 @@ export const DUNGEONS = [{
         monsters: [
           { slime_blue:  1,                 weight: 49.98 },
           { slime_blue:  2,                 weight: 40 },
+          { slime_blue:  3,                 weight: 5 },
           { slime_blue:  8,                 weight: 5 },
-          { slime_blue:  1, slime_green: 1, weight: 5 },
           { slime_dark:  1,                 weight: 0.01 },
           { slime_angel: 1,                 weight: 0.01 }
         ] }, {
@@ -18,7 +18,7 @@ export const DUNGEONS = [{
         monsters: [
           { slime_green: 1, slime_blue: 1, weight: 59.94 },
           { slime_green: 2,                weight: 20 },
-          { slime_green: 1, slime_red: 1,  weight: 20 },
+          { slime_green: 3, slime_red: 1,  weight: 20 },
           { slime_dark:  1,                weight: 0.03 },
           { slime_angel: 1,                weight: 0.03 }
         ]
@@ -28,7 +28,7 @@ export const DUNGEONS = [{
         monsters: [
           { slime_red:  2,                   weight: 49.86 },
           { slime_red:  1, slime_green: 1,   weight: 30 },
-          { slime_red:  1, slime_water: 1,   weight: 20 },
+          { slime_red:  3, slime_water: 1,   weight: 20 },
           { slime_dark:  1,                  weight: 0.07 },
           { slime_angel: 1,                  weight: 0.07 }
         ]
@@ -37,7 +37,7 @@ export const DUNGEONS = [{
         monsters: [
           { slime_red:  1, slime_water: 1,   weight: 49.70 },
           { slime_water: 5,                  weight: 30 },
-          { slime_water: 1, slime_fire: 1,   weight: 20 },
+          { slime_water: 2, slime_fire: 1,   weight: 20 },
           { slime_dark:  1,                  weight: 0.15 },
           { slime_angel: 1,                  weight: 0.15 }
         ]
@@ -46,7 +46,7 @@ export const DUNGEONS = [{
         monsters: [
           { slime_water: 1, slime_fire: 1,   weight: 49.40 },
           { slime_fire:  2,                  weight: 30 },
-          { slime_fire:  1, slime_ice: 1,    weight: 20 },
+          { slime_fire:  2, slime_ice: 1,    weight: 20 },
           { slime_dark:  1,                  weight: 0.3 },
           { slime_angel: 1,                  weight: 0.3 }
         ]
@@ -54,7 +54,7 @@ export const DUNGEONS = [{
         level: 6,
         monsters: [
           { slime_fire:  1, slime_ice: 1,    weight: 49 },
-          { slime_ice:   1, slime_wind: 1,   weight: 20 },
+          { slime_ice:   2, slime_wind: 2,   weight: 20 },
           { slime_ice:   3,                  weight: 15 },
           { slime_fire:  3,                  weight: 15 },
           { slime_dark:  1,                  weight: 0.5 },
@@ -65,7 +65,7 @@ export const DUNGEONS = [{
         monsters: [
           { slime_ice:   1, slime_wind: 1,    weight: 48.4 },
           { slime_wind:  2,                   weight: 30 },
-          { slime_wind:  1, slime_thunder: 1, weight: 20 },
+          { slime_wind:  4, slime_thunder: 1, weight: 20 },
           { slime_dark:  1,                   weight: 0.8 },
           { slime_angel: 1,                   weight: 0.8 }
         ]
@@ -73,7 +73,7 @@ export const DUNGEONS = [{
         level: 8,
         monsters: [
           { slime_wind:    1, slime_thunder: 1, weight: 47.6 },
-          { slime_thunder: 2,                   weight: 30 },
+          { slime_thunder: 5,                   weight: 30 },
           { slime_thunder: 1, slime_flower: 1,  weight: 20 },
           { slime_dark:    1,                   weight: 1.2 },
           { slime_angel:   1,                   weight: 1.2 }
@@ -82,8 +82,8 @@ export const DUNGEONS = [{
         level: 9,
         monsters: [
           { slime_thunder: 1, slime_flower: 1, weight: 46.4 },
-          { slime_flower:  2,                  weight: 30 },
-          { slime_flower:  1, slime_grass: 1,  weight: 20 },
+          { slime_flower:  3,                  weight: 30 },
+          { slime_flower:  3, slime_grass: 3,  weight: 20 },
           { slime_dark:    1,                  weight: 1.8 },
           { slime_angel:   1,                  weight: 1.8 }
         ]
@@ -92,7 +92,7 @@ export const DUNGEONS = [{
         monsters: [
           { slime_flower: 1, slime_grass: 1,  weight: 45 },
           { slime_grass:  2,                  weight: 30 },
-          { slime_grass:  1, slime_flower: 1, slime_thunder: 1, weight: 20 },
+          { slime_grass:  4, slime_flower: 1, slime_thunder: 1, weight: 20 },
           { slime_dark:   1,                  weight: 2.5 },
           { slime_angel:  1,                  weight: 2.5 }
         ]
