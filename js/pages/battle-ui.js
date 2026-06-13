3649769a842030e62844fd6bf01173329d821ee7
euphmat <euphmat@gmail.com>
2026-06-13 10:34:19 +0900

    commit

--- js/pages/battle-ui.js
+++ js/pages/battle-ui.js
@@ -56,7 +56,9 @@ export function renderEnemyCardHtml(e, selectedEnemyTarget) {
 
 export function renderPartyCardHtml(p, activeCharacter, isAutoBattle, selectedPartyMember) {
   let borderClass = 'border-gray-700';
-  if (activeCharacter === p) {
+  const disableAnimations = localStorage.getItem('disableBattleAnimations') === 'true';
+
+  if (activeCharacter === p && !(isAutoBattle && disableAnimations)) {
     borderClass = 'border-yellow-400 shadow-[0_0_8px_rgba(250,204,21,0.5)]';
   } else if (isAutoBattle && selectedPartyMember === p) {
     borderClass = 'border-blue-400 shadow-[0_0_8px_rgba(96,165,250,0.5)]';
