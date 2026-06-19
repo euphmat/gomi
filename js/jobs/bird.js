95767e5e1d180d5843e381828725fb3172444c09
euphmat <euphmat@gmail.com>
2026-06-19 22:29:14 +0900

    commit

--- js/jobs/bird.js
+++ js/jobs/bird.js
@@ -17,7 +17,7 @@ export const bird = {
         { level:  7, spCost: 3, mpCost: 25, chance: 60 },
         { level:  8, spCost: 3, mpCost: 28, chance: 65 },
         { level:  9, spCost: 3, mpCost: 30, chance: 75 },
-        { level: 10, spCost: 5, mpCost: 35, chance: 100 }
+        { level: 10, spCost: 5, mpCost: 35, chance: 80 }
       ],
       getDescription: (levelConfig) => `MP を ${levelConfig.mpCost} 消費し、敵全体を ${levelConfig.chance}％ の確率で睡眠状態にする`,
       execute(caster, levelConfig, battle) {
@@ -104,12 +104,12 @@ export const bird = {
         { level:  2, spCost: 1, mpCost: 12, amount: 20, turns: 3 },
         { level:  3, spCost: 1, mpCost: 14, amount: 25, turns: 3 },
         { level:  4, spCost: 2, mpCost: 18, amount: 30, turns: 4 },
-        { level:  5, spCost: 2, mpCost: 20, amount: 40, turns: 4 },
-        { level:  6, spCost: 2, mpCost: 24, amount: 50, turns: 4 },
-        { level:  7, spCost: 3, mpCost: 28, amount: 60, turns: 5 },
-        { level:  8, spCost: 3, mpCost: 32, amount: 70, turns: 5 },
-        { level:  9, spCost: 3, mpCost: 36, amount: 80, turns: 5 },
-        { level: 10, spCost: 5, mpCost: 50, amount: 100, turns: 5 }
+        { level:  5, spCost: 2, mpCost: 20, amount: 35, turns: 4 },
+        { level:  6, spCost: 2, mpCost: 24, amount: 40, turns: 4 },
+        { level:  7, spCost: 3, mpCost: 28, amount: 45, turns: 5 },
+        { level:  8, spCost: 3, mpCost: 32, amount: 50, turns: 5 },
+        { level:  9, spCost: 3, mpCost: 36, amount: 55, turns: 5 },
+        { level: 10, spCost: 5, mpCost: 50, amount: 60, turns: 5 }
       ],
       getDescription: (levelConfig) => `MP を ${levelConfig.mpCost} 消費し、${levelConfig.turns} ターンの間、味方全体の状態異常耐性を ${levelConfig.amount} 上昇させる`,
       execute(caster, levelConfig, battle) {
