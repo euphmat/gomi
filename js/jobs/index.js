19fccd39bfcc5f6cb7f3446127826eea95a262e8
euphmat <euphmat@gmail.com>
2026-06-23 04:17:49 +0900

    commit

--- js/jobs/index.js
+++ js/jobs/index.js
@@ -29,13 +29,13 @@ export const JOB_STAT_MULTIPLIER = {
   [knight.id]:       { hp: 2.0, mp: 0.8, atk: 1.1, def: 1.5, matk: 0.1, mdef: 0.4, spd: 0.9 },
   [mage.id]:         { hp: 0.5, mp: 1.6, atk: 0.1, def: 0.8, matk: 2.0, mdef: 1.2, spd: 0.6 },
   [priest.id]:       { hp: 0.7, mp: 1.5, atk: 0.2, def: 0.8, matk: 1.2, mdef: 1.2, spd: 0.9 },
-  [ranger.id]:       { hp: 1.0, mp: 1.0, atk: 1.0, def: 0.9, matk: 0.6, mdef: 0.8, spd: 1.2 },
+  [ranger.id]:       { hp: 1.0, mp: 1.0, atk: 1.0, def: 0.9, matk: 0.6, mdef: 0.8, spd: 1.1 },
   [magic_knight.id]: { hp: 0.9, mp: 1.3, atk: 1.0, def: 0.8, matk: 1.2, mdef: 0.9, spd: 0.6 },
   [slime_master.id]: { hp: 1.5, mp: 1.8, atk: 0.7, def: 1.3, matk: 0.8, mdef: 1.1, spd: 0.5 },
   [dancer.id]:       { hp: 0.8, mp: 1.3, atk: 1.0, def: 0.7, matk: 1.3, mdef: 0.7, spd: 1.1 },
   [bird.id]:         { hp: 0.8, mp: 1.5, atk: 0.5, def: 1.0, matk: 0.9, mdef: 1.5, spd: 0.8 },
-  [black_knight.id]: { hp: 3.0, mp: 0.3, atk: 1.3, def: 1.0, matk: 0.8, mdef: 0.3, spd: 0.7 },
-  [paladin.id]:      { hp: 2.0, mp: 1.2, atk: 0.8, def: 1.0, matk: 0.8, mdef: 1.5, spd: 0.5 }
+  [black_knight.id]: { hp: 3.0, mp: 0.3, atk: 1.6, def: 1.0, matk: 1.0, mdef: 0.3, spd: 0.7 },
+  [paladin.id]:      { hp: 2.0, mp: 1.2, atk: 0.8, def: 1.2, matk: 0.8, mdef: 1.5, spd: 0.5 }
 };
 
 export const JOBS = {
