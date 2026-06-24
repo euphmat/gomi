b7c1f89eb42a178a408026ea692ee0887a13388f
euphmat <euphmat@gmail.com>
2026-06-24 21:21:45 +0900

    commit

--- js/pages/guild-tabs/ranch.js
+++ js/pages/guild-tabs/ranch.js
@@ -836,7 +836,10 @@ async function showFeedModal(container, dungeonId, monsterId, monsterDef, monste
             <span class="material-symbols-outlined text-[16px]">group_add</span> ボーナスについて
           </h4>
           <p class="text-xs leading-relaxed text-slate-400">
-            パーティ編成時、牧場で育てた全モンスターのステータスの <span class="text-white font-bold">10%</span>（HPのみ <span class="text-white font-bold">1%</span>）がパーティ全体のボーナスとして加算されます。<br>色々なモンスターを育てて冒険を有利に進めましょう！
+            パーティ編成時、牧場で育てた全モンスターからパーティ全体にボーナスが加算されます。<br>
+            <span class="text-pink-200 font-bold">【基本恩恵】</span>各ステータスの <span class="text-white font-bold">1%</span>（最低保証+1）<br>
+            <span class="text-pink-200 font-bold">【レベル恩恵】</span>レベルが上がった分（Lv-1）だけ、SPD以外に <span class="text-white font-bold">+1</span> ずつ追加<br>
+            色々なモンスターを育てて冒険を有利に進めましょう！
           </p>
         </div>
       </div>
